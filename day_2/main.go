44b0d1a45d7630e7dd329aac4b887f1eac273c9a
Titanio Yudista <titaniy98@gmail.com>
2022-09-14 13:28:46 +0700

    RESTful API Book and User

--- day_2/main.go
+++ day_2/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"day_2/routes"
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	port := ":8080"
+	fmt.Printf("Successfully started on port %s\n", port)
+	routes.Routes()
+	e := echo.New()
+	e.Logger.Fatal(e.Start(":8080"))
+}
